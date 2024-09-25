465cb968a0aeb212f062ecf2d20e204b464dacbf
Navindu <dahanayakenw.20@uom.lk>
2024-09-25 06:34:55 +0530

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+   title: {
+      template: "%s | Acme Dashboard",
+      default: "Acme Dashboard",
+   },
+   description: "A financial dashboard , built with app router",
+   metadataBase: new URL("https://financial-dashboard-mu-jade.vercel.app/"),
+};
 
 export default function RootLayout({
    children,
